60f264d5da287c79ca1f32f40ee951a31674a8be
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-14 17:48:49 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,3 +1,7 @@
 def current_age_for_birth_year(birth_year)
+<<<<<<< HEAD
    Time.now.year - birth_year
+=======
+ return   Time.now.year - birth_year
+>>>>>>> 6db5f451b76a477a9a68ac761450950a53f9ead4
 end
--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -12,7 +12,11 @@ require_relative '../current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
+<<<<<<< HEAD
     now = Time.new("2018-01-01 13:00:00")
+=======
+    now =  Time.now.year
+>>>>>>> 6db5f451b76a477a9a68ac761450950a53f9ead4
     allow(Time).to receive(:now) { now }
     age_of_person = current_age_for_birth_year(1998)
 
